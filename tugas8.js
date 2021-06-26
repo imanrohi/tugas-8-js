740356fbb02d292f2d776b35881e35f8eb42f812
User-niomic <user@niomic.com>
2021-06-26 11:25:27 +0700

    Tugas 8 Javascripts
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas8.js
@@ -0,0 +1,14 @@
+function panggilObjects() {
+  var mobil={
+    type: 'Sedan',
+    harga: 200000000,
+    warna: 'Putih',
+  }
+  console.log(mobil)
+  mobil.harga=190000000000
+  mobil.tahun=2020
+  console.log(mobil)
+  console.log(mobil.harga)
+}
+
+panggilObjects()
